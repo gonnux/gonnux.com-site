aff289803cae00f3a72c7e8817bda90be12d02b6
Byeonggon Lee <gonny952@gmail.com>
2020-12-22 21:39:51 +0900

    Refactor

--- components/withLayout.jsx
+++ components/withLayout.jsx
@@ -1,10 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles'
 import AppBar from './AppBar'
+import Container from '@material-ui/core/Container'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(2),
+  },
+}))
 
 export default function withLayout(Component) {
-  return (props) => (
-    <>
+  return (props) => {
+    const classes = useStyles()
+    return <>
       <AppBar />
-      <Component {...props} />
+      <Container className={classes.container} maxWidth="sm">
+        <Component {...props} />
+      </Container>
     </>
-  )
+  }
 }
---
+++ pages/404.jsx
@@ -0,0 +1,15 @@
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import AppCard from '../components/AppCard'
+import withLayout from '../components/withLayout'
+
+function Apps() {
+  return (
+    <Typography align="center" variant="h4">
+      404 Not Found
+    </Typography>
+  )
+}
+
+export default withLayout(Apps)
