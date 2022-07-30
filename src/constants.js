2fd82592db47a5bbb72e5b9953f700814b56d7db
Byeonggon Lee <binkoni@gonnux.com>
2022-07-30 21:55:18 +0900

    Remove hard-coded values

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
+export const DATA_YAML = 'data.yaml'
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,11 +1,12 @@
 import withLayout from '../components/withLayout'
 import ProjectList from '../components/ProjectList'
+import { DATA_YAML } from '../constants'
 
 export async function getStaticProps() {
   const yaml = require('js-yaml')
   const fs = require('fs')
 
-  const { projects } = yaml.safeLoad(fs.readFileSync('data.yaml', 'utf8'))
+  const { projects } = yaml.safeLoad(fs.readFileSync(DATA_YAML, 'utf8'))
 
   return {
     props: { projects },
