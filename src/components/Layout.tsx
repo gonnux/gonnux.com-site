a14de623508a554d9331c887bb6bbd2e92dc357f
Byeonggon Lee <binkoni@gonnux.com>
2023-02-11 00:38:29 +0900

    refactor: Separate Footer from Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,9 @@
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Divider from '@mui/material/Divider'
-import Typography from '@mui/material/Typography'
-import Link from '@mui/material/Link'
-import AppBar from './AppBar'
 import { FC, ReactNode } from 'react'
+import AppBar from './AppBar'
+import Footer from './Footer'
 
 // https://dev.to/ofilipowicz/next-js-per-page-layouts-and-typescript-lh5
 const PREFIX = 'Layout';
@@ -28,32 +26,17 @@ const Root = styled('div')((
 }));
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
-  const githubRemoteUrl = process.env.NEXT_PUBLIC_GITHUB_REMOTE_URL
-  const gitCommitHash = process.env.NEXT_PUBLIC_GIT_COMMIT_HASH
-  const githubCommitUrl = `${githubRemoteUrl}/commit/${gitCommitHash}`
-
   return (
-    (<Root>
+    <Root>
       <AppBar />
       <Divider />
       <Container className={classes.container} maxWidth="md">
         { children }
       </Container>
       <Divider />
-      <Box display="flex" justifyContent="center">
-        <Typography>
-          <Link href={ githubCommitUrl }>
-            { gitCommitHash }
-          </Link>
-        </Typography>
-      </Box>
-      <Box display="flex" justifyContent="center">
-        <Typography>
-          &copy; { new Date().getFullYear() } Gonnux. All Right Reserved.
-        </Typography>
-      </Box>
-    </Root>)
-  );
+      <Footer />
+    </Root>
+  )
 }
 
 export default Layout
